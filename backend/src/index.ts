fb3bcba67d715435554829dd6a05f49ef0b0080f
Muhammad Azizi <azizi@hotmail.com.my>
2021-05-06 01:18:43 +0800

    + Handle cors error on allow origin header

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import details from '../routers/getDetails';
 import cupcake from '../routers/cupcake';
 
+const corsOptions = {
+  origin: 'http://localhost:8080'
+};
+
 const app = express();
 const port = 3000;
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send('Yes I did in ts file!');
