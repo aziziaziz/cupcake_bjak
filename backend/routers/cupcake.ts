12c70a3d1b58af9820c2222b482e90cb80cc028c
Azizi Aziz <azizi@dsm.com.my>
2021-05-06 10:26:26 +0800

    + Added method to insert and get cupcakes from db

--- backend/routers/cupcake.ts
+++ backend/routers/cupcake.ts
@@ -1,22 +1,54 @@
 import express from 'express';
 import mongoConn from '../modules/mongo';
-import fileReader from 'fs';
 
 const cup = require('../files/defaultCupcake.json');
 
-const conn = new mongoConn('test', 'cupcake');
+const listingConn = new mongoConn('cupcake', 'cupcakeListing');
 
 const app = express.Router();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
 app.use(express.raw());
 
-app.get('/', getCupCakes);
-// app.get('/getDetails', getDetails);
-// app.delete('/deleteDetails', deleteDetails);
+app.get('/', getDefaultCupCakes);
+app.post('/insert', insert);
+app.get('/get', getAllCupcakes);
 
-async function getCupCakes(req, res) {
+async function getDefaultCupCakes(req, res) {
   res.send(cup);
+}
+
+async function insert(req, res) {
+  var currentUser = await getAllCupcakes(null, null);
+  var hasUser = currentUser.filter(c => c['name'].toLowerCase() == req.body['name'].toLowerCase());
+  if (hasUser.length > 0) {
+    res.status(400);
+    res.send({ inserted: false, insertCount: 0, message: `Cannot insert ${req.body['name']}. ${req.body['name']} already exist.` });
+  } else {
+    var mongo = await listingConn.getCollection();
+    var result = await mongo.insertOne(req.body);
+    
+    if (result.insertedCount > 0) {
+      res.send({ inserted: true, insertCount: result.insertedCount, message: `Inserted ${result.insertedCount} cupcake${result.insertedCount > 0 ? 's' : ''}` });
+    } else {
+      res.send({ inserted: false, insertCount: result.insertedCount, message: `Failed to insert cupcake into db. Please try again.` });
+    }
+  }
+}
+
+async function getAllCupcakes(req, res) {
+  const mongo = await listingConn.getCollection();
+  var dbResult = await mongo.find();
+
+  var result = [];
+  await dbResult.forEach(r => result.push(r));
+  // result.forEach(r => delete r['_id']);
+
+  if (res) {
+    res.send(result);
+  } else {
+    return result;
+  }
 }
 
 export default app;
