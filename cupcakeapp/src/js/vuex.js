f5f67b70f853eaf0e70491825c1886caeb4bf2b9
Azizi Aziz <azizi@dsm.com.my>
2021-05-06 16:26:24 +0800

    + Improve the mutations in the vuex to count the total and the cart

--- cupcakeapp/src/js/vuex.js
+++ cupcakeapp/src/js/vuex.js
@@ -5,13 +5,32 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    cartCount: 0
+    cupcakeListing: [],
+    groupedListing: [],
+    cartCount: 0,
+
+    subtotal: 0,
+    sst: 0,
+    total: 0
   },
   getters: {
   },
   mutations: {
-    async startSignal (state) {
-      console.log(state);
+    countCart (state) {
+      var cartCount = 0;
+      var carts = state.groupedListing.map(c => c['qty']);
+      if (carts.length > 0) {
+        cartCount = carts.reduce((a, b) => { return a + b });
+      }
+      state.cartCount = cartCount;
+    },
+    countTotal (state) {
+      var total = 0;
+      state.groupedListing.forEach(c => total += (c['qty'] * c['cupcake']['price']));
+      
+      state.subtotal = total;
+      state.sst = total * 0.06;
+      state.total = state.subtotal + state.sst;
     }
   }
 })
