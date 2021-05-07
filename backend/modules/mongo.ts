8f2b0a515531be0c2e5dae4bd31d16b5212461a0
Azizi Aziz <azizi@dsm.com.my>
2021-05-07 10:13:38 +0800

    + Update to remove all warnings for mongo

--- backend/modules/mongo.ts
+++ backend/modules/mongo.ts
@@ -2,26 +2,31 @@ import mongo from 'mongodb';
 
 const mongoClient = mongo.MongoClient;
 const url = "mongodb://127.0.0.1:27017";
-const client = new mongoClient(url, { useUnifiedTopology: true });
-
-var db = null;
-var collection = null;
+var client = null;
 
 class mongoConnect {
+  db: String;
+  collection: String;
+
   constructor(dbName: String, collectionName: String) {
-    db = dbName;
-    collection = collectionName;
+    this.db = dbName;
+    this.collection = collectionName;
   }
 
   async getCollection() {
     try {
+      client = new mongoClient(url, { useUnifiedTopology: true });
       await client.connect();
-      const database = client.db(db);
-      return database.collection(collection);
+      const database = client.db(this.db);
+      return database.collection(this.collection);
     } catch (e) {
       client.close();
       return null;
     }
+  }
+
+  close() {
+    client.close();
   }
 }
 
